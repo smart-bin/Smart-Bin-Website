8fba5713f7a80c16861860f2d5df82e34201912e
Ian Wensink <ian@burst-digital.com>
2016-01-21 21:41:50 +0100

    Added sub navigation support

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+function initWebsite() {
+}
+
 function getUserId() {
     if (typeof localStorage.userId == "undefined") return 2;
     else return localStorage.userId;
+}
+
+function toggleSubmenu() {
+    var menu = $(this).find(".has-submenu");
+    $("#"+ menu.attr("for")).toggleClass("hidden");
 }
